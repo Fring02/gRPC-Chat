4a73d778837ac558678384a4c44e8b13ed572651
Sultan <hasenovsultanbek@gmail.com>
2021-01-14 16:48:00 +0600

    Add chat gRPC client and server

---
+++ ChatService/Services/ChatRoomService.cs
@@ -0,0 +1,78 @@
+﻿using ChatService.Models;
+using ChatService.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatService
+{
+    public class ChatRoomService
+    {
+        private readonly MessagesRepository _messages;
+        private ILogger<ChatRoomService> _logger;
+        private static readonly ConcurrentDictionary<string, IServerStreamWriter<Message>> users =
+            new ConcurrentDictionary<string, IServerStreamWriter<Message>>();
+        public ChatRoomService(MessagesRepository messages, ILogger<ChatRoomService> logger)
+        {
+            _messages = messages;
+            _logger = logger;
+        }
+        public async Task Join(string userName, ChatRoom chatRoom, IServerStreamWriter<Message> response)
+        {
+            if (users.TryAdd(userName, response))
+            {
+                var chatRoomId = Guid.Parse(chatRoom.Id);
+                if (!await _messages.HasUser(chatRoomId, userName))
+                {
+                    _logger.LogInformation($"Creating entering message for user {userName} on chat room {chatRoom.Name}");
+                    await response.WriteAsync(new Message { Text = " has entered the chat!", User = userName });
+                }
+                else _messages.HistoryMessages(Guid.Parse(chatRoom.Id)).Result.ForEach(m => response.WriteAsync(m));
+            }
+        }
+
+
+        public void Remove(string name) => users.TryRemove(name, out _);
+
+        public async Task BroadcastMessageAsync(Message message) => await BroadcastMessage(message);
+
+        private async Task BroadcastMessage(Message message)
+        {
+            var messageModel = new MessageModel { User = message.User, Text = message.Text,
+                ChatRoomId = Guid.Parse(message.ChatRoomId) };
+            if(await _messages.AddMessage(messageModel))
+            {
+                foreach (var user in users.Where(x => x.Key != message.User))
+                {
+                  await SendMessageToSubscriber(user, message);
+                }
+            }
+            else
+            {
+                message.Text = "Failed to add message";
+                _logger.LogError(message.Text);
+                await SendMessageToSubscriber(users.FirstOrDefault(u => u.Key == message.User), message);
+            } 
+        }
+
+        private async Task<KeyValuePair<string, IServerStreamWriter<Message>>?> SendMessageToSubscriber(
+            KeyValuePair<string, IServerStreamWriter<Message>> pair, Message message)
+        {
+            try
+            {
+                await pair.Value.WriteAsync(message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return pair;
+            }
+        }
+    }
+
+}
---
+++ GrpcClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using ChatClient;
+using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var username = Console.ReadLine();
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ChatService.ChatServiceClient(channel);
+            foreach(var chatRoom in client.GetChatRoomsAsync(new Lookup()).ResponseAsync.Result.ChatRooms_)
+            {
+                Console.WriteLine(chatRoom.Id + ": " + chatRoom.Name);
+            }
+            using (var chat = client.JoinAndWriteMessage())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext())
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User} : {response.Text}");
+                    }
+                });
+                Console.WriteLine("Enter id of chat room to join");
+                var preferredChatRoom = Console.ReadLine();
+                if (!string.IsNullOrEmpty(preferredChatRoom))
+                {
+                    Console.Clear();
+                    await chat.RequestStream.WriteAsync(new Message { User = username, ChatRoomId = preferredChatRoom });
+                }
+                string messageText;
+                while ((messageText = Console.ReadLine()) != null)
+                {
+                    if (messageText.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+                    await chat.RequestStream.WriteAsync(new Message { User = username, Text = messageText, ChatRoomId = preferredChatRoom });
+                }
+
+                await chat.RequestStream.CompleteAsync();
+            }
+
+            Console.WriteLine("Disconnection started!");
+            await channel.ShutdownAsync();
+        }
+    }
+}
